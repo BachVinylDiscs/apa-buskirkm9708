31f356fcc65057de5e6873d3fe8e51e1ac6d2614
Max Buskirk <buskirk.max@gmail.com>
2019-03-04 22:44:50 +0000

    Finished ListOddToEven

--- U9B-arraylist/ListOddToEven.java
+++ U9B-arraylist/ListOddToEven.java
@@ -1,15 +1,39 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Max Buskirk
+//Date - March 4, 2019
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListOddToEven
 {
-  public static boolean go( List<Integer> ray )
+  public static int go( List<Integer> ray )
   {
-    return false;
+    int result=0;
+    int odd=-1;
+    for(int i=0;i<ray.size();i++){
+      if(ray.get(i)%2==1){
+        odd+=(i+1);
+        break;
+      }
+    }
+    if(odd>-1){
+      int even=-1;
+      for(int j=ray.size()-1;j>=odd;j--){
+        if(ray.get(j)%2==0){
+          even+=(j+1);
+          break;
+        }
+      }
+      if(even>-1){
+        result=Math.abs(odd-even);
+      }else{
+        result=-1;
+      }
+    }else{
+      result=-1;
+    }
+    return result;
   }
 }
--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -2,10 +2,18 @@
 //www.apluscompsci.com
 //Name -
 //Date -
-
+import java.util.ArrayList;
+import java.util.List;
 public class OddToEvenRunner
 {
   public static void main( String args[] )
   {
+    ArrayList<Integer> arrl;
+    arrl=new ArrayList<Integer>();
+    int[] arr={7,1,5,3,11,5,6,7,8,9,10,12345,11};
+    for(int i:arr){
+      arrl.add(i);
+    }
+    System.out.println(ListOddToEven.go(arrl));
   }
 }
