8ea74b9f96715c5b0cb9beecec1a60124ddae1fb
Max Buskirk <buskirk.max@gmail.com>
2019-03-04 22:57:23 +0000

    Finished ListDown

--- U9B-arraylist/DownRunner.java
+++ U9B-arraylist/DownRunner.java
@@ -1,11 +1,19 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
-
+//Name - Max Buskirk
+//Date - March 4, 2019
+import java.util.ArrayList;
+import java.util.List;
 public class DownRunner
 {
   public static void main( String args[] )
-  {											
+  {
+    ArrayList<Integer> arrl;
+    arrl=new ArrayList<Integer>();
+    int[] arr={250,19,17,15,13,11,10,9,6,3,2,1,-455};
+    for(int i:arr){
+      arrl.add(i);
+    }
+    System.out.println(ListDown.go(arrl));
   }
 }
