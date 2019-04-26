44a21f3c9ea87daca5444a458277192c0fc4b5ef
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-26 15:28:23 -0700

    finished Score.java

--- ponglab/act4/Score.java
+++ ponglab/act4/Score.java
@@ -1,1 +1,28 @@
-public
+//package ponglab.act4;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Max Buskirk
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Score
+{
+  private int score;
+  
+  public Score()
+  {
+    score=0;
+  }
+  
+  public paint(Graphics window)
+  {
+    window.drawString("Score: "+score,50,10);
+  }
+  
+  public addPoint(int x)
+  {
+    score+=x;
+  }
+}
