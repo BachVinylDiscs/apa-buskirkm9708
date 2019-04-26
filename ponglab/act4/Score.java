c0e283c270a53121a7ebbf0e2b543c9c901add13
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-26 15:56:30 -0700

    fixed score

---
+++ ponglab/act4/Score.java
@@ -0,0 +1,55 @@
+//package ponglab.act4;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Max Buskirk
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Score
+{
+  private int score;
+  private int xPos;
+  private int yPos;
+  
+  public Score()
+  {
+    score=0;
+    xPos=50;
+    yPos=10;
+  }
+  
+  public Score(int x,int y)
+  {
+    score=0;
+    xPos=x;
+    yPos=y;
+  }
+  
+  public void draw(Graphics window)
+  {
+    window.setColor(Color.BLACK);
+    window.drawString("Score: "+score,xPos,yPos);
+  }
+  
+  public void draw(Graphics window,Color col)
+  {
+    window.setColor(col);
+    window.drawString("Score: "+score,xPos,yPos);
+  }
+  
+  public addScore(Graphics window,int x)
+  {
+    window.setColor(Color.WHITE);
+    window.drawString("Score: "+score,xPos,yPos);
+    score+=x;
+  }
+  
+  public setScore(Graphics window,int x)
+  {
+    window.setColor(Color.WHITE);
+    window.drawString("Score: "+score,xPos,yPos);
+    score=x;
+  }
+}
