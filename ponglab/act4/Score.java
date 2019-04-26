098dad14634dba08340e694d763da5fbda80bd57
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-26 15:53:01 -0700

    fixed score draw

--- ponglab/act4/Score.java
+++ ponglab/act4/Score.java
@@ -10,35 +10,46 @@ import java.awt.Graphics;
 public class Score
 {
   private int score;
+  private int xPos;
+  private int yPos;
   
   public Score()
   {
     score=0;
+    xPos=50;
+    yPos=10;
   }
   
-  public void draw(Graphics window,int x,int y)
+  public Score(int x,int y)
+  {
+    score=0;
+    xPos=x;
+    yPos=y;
+  }
+  
+  public void draw(Graphics window)
   {
     window.setColor(Color.BLACK);
-    window.drawString("Score: "+score,x,y);
+    window.drawString("Score: "+score,xPos,yPos);
   }
   
-  public void draw(Graphics window,int x,int y,Color col)
+  public void draw(Graphics window,Color col)
   {
     window.setColor(col);
-    window.drawString("Score: "+score,x,y);
+    window.drawString("Score: "+score,xPos,yPos);
   }
   
   public addScore(int x)
   {
     window.setColor(Color.WHITE);
-    window.drawString("Score: "+score,x,y);
+    window.drawString("Score: "+score,xPos,yPos);
     score+=x;
   }
   
   public setScore(int x)
   {
     window.setColor(Color.WHITE);
-    window.drawString("Score: "+score,x,y);
+    window.drawString("Score: "+score,xPos,yPos);
     score=x;
   }
 }
