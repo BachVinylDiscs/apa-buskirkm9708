ee04285379693eb055910975e28229239fe48f89
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-26 14:38:56 -0700

    completed wall.java

--- ponglab/act4/Wall.java
+++ ponglab/act4/Wall.java
@@ -1,1 +1,16 @@
-public
+package ponglab.act4;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Wall extends Block
+{
+  public Paddle()
+  {
+    super(0,0,800,1);
+  }
+}
