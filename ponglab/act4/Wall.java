20642537860046e956525d8ca665a52ce2154efb
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-28 12:33:19 -0700

    constructor purge

--- ponglab/act4/Wall.java
+++ ponglab/act4/Wall.java
@@ -9,16 +9,6 @@ import java.awt.Graphics;
 
 public class Wall extends Block
 {
-  public Wall()
-  {
-    super(0,0,800,1);
-  }
-
-  public Wall(int x,int y)
-  {
-    super(x,y,800,1);
-  }
-
   public Wall(int x,int y,int w,int h)
   {
     super(x,y,w,h);
