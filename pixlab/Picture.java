df6cb7f232d40608223618109311961b915ed946
BachVinylDiscs <buskirk.max@gmail.com>
2019-04-30 21:47:16 +0000

    finished act6

--- pixlab/Picture.java
+++ pixlab/Picture.java
@@ -224,6 +224,73 @@ public class Picture extends SimplePicture
     } 
   }
   
+  public void mirrorVerticalRightToLeft()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel leftPixel = null;
+    Pixel rightPixel = null;
+    int width = pixels[0].length;
+    for (int row = 0; row < pixels.length; row++)
+    {
+      for (int col = width/2; col < width; col++)
+      {
+        leftPixel = pixels[row][col];
+        rightPixel = pixels[row][width - 1 - col];
+        rightPixel.setColor(leftPixel.getColor());
+      }
+    } 
+  }
+  
+  public void mirrorHorizontal()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel topPixel = null;
+    Pixel bottomPixel = null;
+    int height = pixels.length;
+    for (int row = 0; row < height/2; row++)
+    {
+      for (int col = 0; col < pixels[row].length; col++)
+      {
+        topPixel = pixels[row][col];
+        bottomPixel = pixels[height - 1 - row][col];
+        bottomPixel.setColor(topPixel.getColor());
+      }
+    }
+  }
+  
+  public void mirrorHorizontalBotToTop()
+  {
+    Pixel[][] pixels = this.getPixels2D();
+    Pixel topPixel = null;
+    Pixel bottomPixel = null;
+    int height = pixels.length;
+    for (int row = height / 2; row < height; row++)
+    {
+      for (int col = 0; col < pixels[row].length; col++)
+      {
+        topPixel = pixels[row][col];
+        bottomPixel = pixels[height - 1 - row][col];
+        bottomPixel.setColor(topPixel.getColor());
+      }
+    }
+  }
+  
+  public void mirrorDiagonal()
+  {
+    Pixel[][] pixels=this.getPixels2D();
+    Pixel blPixel=null;
+    Pixel trPixel=null;
+    for(int row=0;row<pixels.length;row++)
+    {
+      for(int col=0;col<row;col++)
+      {
+        blPixel=[row][col];
+	trPixel=[col][row];
+	trPixel.setColor(blPixel.getColor());
+      }
+    }
+  }
+  
   /** Mirror just part of a picture of a temple */
   public void mirrorTemple()
   {
