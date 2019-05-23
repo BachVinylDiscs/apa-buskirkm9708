b99c8f01e16dd67d0dd7c43a2415b5a260e4e276
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-23 16:24:22 -0700

    Update BlackJackCard.java

--- bjacklab/BlackJackCard.java
+++ bjacklab/BlackJackCard.java
@@ -5,17 +5,25 @@
 
 public class BlackJackCard extends Card
 {
-  //constructors
+  public BlackJackCard()
+  {
+    super();
+  }
 
-
+  public BlackJackCard(int n,String s)
+  {
+    super(n,s);
+  }
 
   public int getValue()
   {
-    //enables you to build the value for the game into the card
-    //this makes writing the whole program a little easier
-
-
-    return 0;
+    if(getFace()==1){
+      return 11;
+    }
+    if(getFace()>=11&&getFace()<=13){
+      return 10;
+    }
+    return getFace();
   }
         
 }
