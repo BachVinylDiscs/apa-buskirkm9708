cdd74eff6f70117798669ba680d8202df3f61018
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-23 16:23:42 -0700

    Update Card.java

--- bjacklab/Card.java
+++ bjacklab/Card.java
@@ -3,40 +3,56 @@
 //Class -
 //Lab  - 
 
-
-public abstract class Card
+public class Card
 {
-  public static final String FACES[] = {"ZERO","ACE","TWO","THREE","FOUR",
-                                        "FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
+  public static final String FACES[]={"ZERO","ACE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE","TEN","JACK","QUEEN","KING"};
 
-  private String suit;
   private int face;
+  private String suit;
 
-  //constructors
+  public Card()
+  {
+    face=0;
+    suit="";
+  }
 
+  public Card(int n,String s)
+  {
+    face=n;
+    suit=s;
+  }
 
+  public void setFace(int n)
+  {
+    face=n;
+  }
 
-  // modifiers
+  public void setSuit(String s)
+  {
+    suit=s;
+  }
 
+  public int getFace()
+  {
+    return face;
+  }
 
- 
-
-  //accessors
-
-
-
-
+  public String getSuit()
+  {
+    return suit;
+  }
 
   public abstract int getValue();
 
   public boolean equals(Object obj)
   {
-
-
-
-    return false;
+    Card other=(Card)obj;
+    return face==other.getFace()&&suit==other.getSuit();
   }
 
-  //toString
-        
+  public String toString()
+  {
+    return FACES[face]+" of "+getSuit()+" | value = "+getValue();
+  }
+
 }
